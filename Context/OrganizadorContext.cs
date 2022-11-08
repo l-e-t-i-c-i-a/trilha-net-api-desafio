c2f0f328fe15a58f9998c427578386581bd27201
l-e-t-i-c-i-a <proj3705@gmail.com>
2022-11-08 20:29:09 -0300

    desafio concluido

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Models;
+
 
 namespace TrilhaApiDesafio.Context
 {
